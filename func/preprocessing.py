1fb5d9c66bf01cb58ecb1ff5efbc6b5b3d6aa6bc
Kevin Spurk <kevin.lee.spurk@gmx.de>
2021-09-15 13:34:05 +0200

    git organisation

---
+++ func/preprocessing.py
@@ -0,0 +1,9 @@
+# data pre-processing functions incl. cleaning, eda, feature engineering
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+import scipy.stats as stats
+import datetime
+
